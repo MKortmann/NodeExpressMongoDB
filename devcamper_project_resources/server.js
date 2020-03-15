80225d1bca08e66815e5517d69d072fa7ed6420d
MKortmann <companymvms@gmail.com>
2020-03-15 12:32:59 -0300

    add: using the xpress router

--- devcamper_project_resources/server.js
+++ devcamper_project_resources/server.js
@@ -3,40 +3,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// router files
+const bootcamps = require("./routes/bootcamps");
+
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Mount routers with the address! So in the bootcamp.js we only need to write "/"
+app.use("/api/v1/bootcamps", bootcamps);
 
 // app.get("/", (req, res) => {
 //   // res.send("<h1>Hello from express</h1>");
 //   // res.json({ name: "Brad" });
 //   res.status(200).json({ success: true, data: { id: 1 } });
 // });
-
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
-});
-
-app.get("/api/v1/bootcamps/:id", (req, res) => {
-  res.status(200).json({ success: true, msg: "get bootcamp bootcamp" });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
-});
-
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(
