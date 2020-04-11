a2e6a2a2326fe2583ff9afc5e77b1fab14697439
MKortmann <companymvms@gmail.com>
2020-04-11 15:46:37 +0200

    add: sign&get json web token

--- devcamper_project_resources/controllers/auth.js
+++ devcamper_project_resources/controllers/auth.js
@@ -16,7 +16,10 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
+  // Create token
+  const token = user.getSignedJwtToken();
+
   console.log(user.red);
 
-  res.status(200).json({ success: true });
+  res.status(200).json({ success: true, token });
 });
