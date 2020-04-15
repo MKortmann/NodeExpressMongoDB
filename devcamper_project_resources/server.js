c18d746cde7beb0e7216cfe04210c06071be77cf
MKortmann <companymvms@gmail.com>
2020-04-15 17:59:50 +0200

    add: XSS Protection & Security Headers

--- devcamper_project_resources/server.js
+++ devcamper_project_resources/server.js
@@ -9,6 +9,8 @@ const colors = require("colors");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -43,6 +45,12 @@ app.use(fileupload());
 
 // Sanitize data: security to avoid NoSQL injections
 app.use(mongoSanitize());
+
+// Helmet: improve security
+app.use(helmet());
+
+// XSS: prevent XSS attacks
+app.use(xss());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
