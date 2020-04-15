1be077ad65590f90aa43ae95d84374cba7b83186
MKortmann <companymvms@gmail.com>
2020-04-15 18:11:08 +0200

    rate limiting HPP & CORS

--- devcamper_project_resources/server.js
+++ devcamper_project_resources/server.js
@@ -8,9 +8,14 @@ const morgan = require("morgan");
 const colors = require("colors");
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+// security packages
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
+const cors = require("cors");
+// --end of security packages
 const errorHandler = require("./middleware/error");
 const connectDB = require("./config/db");
 
@@ -51,6 +56,20 @@ app.use(helmet());
 
 // XSS: prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, //10 mins
+  max: 100,
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
